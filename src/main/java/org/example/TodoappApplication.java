835a332ffbb7b207cff0d356257c820c036b179a
heberalves1 <heber.alves@desktop.tec.br>
2024-07-30 11:50:08 -0300

    Initial commit

---
+++ src/main/java/org/example/TodoappApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+package io.github.Heber2118;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoappApplication.class, args);
+    }
+}
